8804c2c1316537942772a519763edf8118487435
Fortpsina <130063152+Fortpsina@users.noreply.github.com>
2025-04-26 21:47:22 +0300

    Update utils.py

--- plugins/utils.py
+++ plugins/utils.py
@@ -1,16 +1,3 @@
 '''
-Utils for mas.
+There will be utils for mas. (or not)
 '''
-
-
-def crypt_anon_id(id: int) -> str:
-    return str(id*7)[:-1]
-
-
-def decrypt_anon_id(id: int) -> int:
-    return int(str(id/7)[:-1])
-
-
-if __name__ == "__main__":
-    print(crypt_anon_id(428192863))
-    print(crypt_anon_id(299735004))
