e5fa96ef99021ef2a32ec0bf017567f7c596402e
Fortpsina <130063152+Fortpsina@users.noreply.github.com>
2025-06-12 02:13:54 +0300

    Update markups.py

--- plugins/markups.py
+++ plugins/markups.py
@@ -4,23 +4,6 @@ from .user import UserProfile
 class _TypeOfSorting:
     def __init__(self): pass
 
-def main_help_markup(admin_version: bool = True) -> InlineKeyboardMarkup:
-    help_menu = InlineKeyboardMarkup(row_width = 1)
-
-    help_menu.add(InlineKeyboardButton(text = 'Команды для расписания', callback_data = 'help_menu_schedule'))
-    if admin_version:
-        help_menu.add(InlineKeyboardButton(text = 'Меню заданий', callback_data = 'help_menu_plan'))
-        help_menu.add(InlineKeyboardButton(text = 'Административные команды', callback_data = 'help_menu_admin'))
-    help_menu.add(InlineKeyboardButton(text = 'Прочие команды', callback_data = 'help_menu_other'))
-
-    return help_menu
-
-def back_to_main_help_markup() -> InlineKeyboardMarkup:
-    back_to_help_menu = InlineKeyboardMarkup(row_width = 1)
-    back_to_help_menu.add(InlineKeyboardButton(text = 'Вернуться к общей справке', callback_data = 'back_to_help_menu'))
-
-    return back_to_help_menu
-
 
 def color_chooser_markup() -> InlineKeyboardMarkup:
     choose_color_default = InlineKeyboardButton (text = 'Стандартный 🟤', callback_data = 'choose_color_default')
@@ -48,57 +31,39 @@ def color_chooser_markup() -> InlineKeyboardMarkup:
     return choose_color
 
 
-def profile_options_markup(own_profile: bool = True, admin_rights: bool = False) -> InlineKeyboardMarkup:
+def profile_options_markup() -> InlineKeyboardMarkup:
     profile_settings_markup = InlineKeyboardMarkup ()
 
-    if own_profile:    
-        profile_settings_markup_name  = InlineKeyboardButton (text = 'Смена имени',  callback_data = 'profile change Name')
-        profile_settings_markup_vk    = InlineKeyboardButton (text = 'Смена ВК',     callback_data = 'profile change VK')
-        profile_settings_markup_group = InlineKeyboardButton (text = 'Смена группы', callback_data = 'profile change Group')
-        profile_settings_markup_color = InlineKeyboardButton (text = 'Цвет',         callback_data = 'profile change Color')
-        
-        profile_settings_markup.row(profile_settings_markup_name, profile_settings_markup_vk)
-        profile_settings_markup.row(profile_settings_markup_group, profile_settings_markup_color)
-
-    if admin_rights:
-        profile_settings_markup_delete_profile = InlineKeyboardButton (text = 'Удалить профиль',  callback_data = 'profile change delete')
-        profile_settings_markup.row(profile_settings_markup_delete_profile)
+    profile_settings_markup_name  = InlineKeyboardButton (text = 'Смена имени',  callback_data = 'profile change Name')
+    profile_settings_markup_vk    = InlineKeyboardButton (text = 'Смена ВК',     callback_data = 'profile change VK')
+    profile_settings_markup_group = InlineKeyboardButton (text = 'Смена группы', callback_data = 'profile change Group')
+    profile_settings_markup_color = InlineKeyboardButton (text = 'Цвет',         callback_data = 'profile change Color')
+    
+    profile_settings_markup.row(profile_settings_markup_name, profile_settings_markup_vk)
+    profile_settings_markup.row(profile_settings_markup_group, profile_settings_markup_color)
 
     return profile_settings_markup
 
 
-def group_chooser_markup() -> InlineKeyboardMarkup:
-    from config import groups
-    
-    group_menu = InlineKeyboardMarkup(row_width = 1)
-    for group in groups:
-        group_menu.add(InlineKeyboardButton(text = group['name'], callback_data = f'group set {group["id"]}'))
-    group_menu.add(InlineKeyboardButton(text = 'Нет моей группы', callback_data = f'group set {1}'))
-
-    return group_menu
-
-
 def select_hs_markup(page: int = 1, sort: _TypeOfSorting | None = None) -> InlineKeyboardMarkup:
-    all_hs = [el for el in range(0, 15)]
+    _nums = ('1️⃣', '2️⃣', '3️⃣', '4️⃣', '5️⃣', '6️⃣', '7️⃣', '8️⃣', '9️⃣', '🔟')
     page_numbers = 1
 
     prev_page = InlineKeyboardButton(text = '⬅️', callback_data = f'hs page {page - 1 if page > 1 else page_numbers}')
-    page_counter = InlineKeyboardButton(text = f'{page} / {page_numbers}', callback_data="...")
     next_page = InlineKeyboardButton(text = '➡️', callback_data = f'hs page {page + 1 if page < page_numbers else 1}')
+    page_counter = InlineKeyboardButton(text = f'{page} / {page_numbers}', callback_data="...")
 
-    hs_add = InlineKeyboardButton(text = 'Добавить вариант', callback_data = f'hs add')
-
-    hs_sortby_popular = InlineKeyboardButton(text = 'По популярности', callback_data = f'hs add')
-    hs_sortby_alphabet = InlineKeyboardButton(text = 'По алфавиту', callback_data = f'hs add')
+    hs_add = InlineKeyboardButton(text = '✏️', callback_data = f'hs add')
+    hs_options = InlineKeyboardButton(text = '⚙️', callback_data = f'hs menu options')
 
     hs_markup = InlineKeyboardMarkup()
 
-    for hs in all_hs:
+    for hs in _nums:
         nigger = InlineKeyboardButton(text=hs, callback_data="...")
         hs_markup.row(nigger)
 
     hs_markup.row(prev_page, page_counter, next_page)
-    hs_markup.row(hs_add)
+    hs_markup.row(hs_add, hs_options)
 
     return hs_markup
 
@@ -110,3 +75,11 @@ def feedback_markup() -> InlineKeyboardMarkup:
     feedback_markup.add(InlineKeyboardButton(text = 'Читать гайд по команде', callback_data = 'feedback guide'))
 
     return feedback_markup
+
+
+def call_data() -> InlineKeyboardMarkup:
+    call_data_markup = InlineKeyboardMarkup(row_width = 1)
+    call_data_markup.add(InlineKeyboardButton(text = 'call', callback_data = 'call'))
+    call_data_markup.add(InlineKeyboardButton(text = 'call data', callback_data = 'call data'))
+
+    return call_data_markup
