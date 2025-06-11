48dcf634656ff2142e3bcda87470a1f1013a3105
Fortpsina <130063152+Fortpsina@users.noreply.github.com>
2025-06-12 02:13:18 +0300

    Update utils.py

--- plugins/utils.py
+++ plugins/utils.py
@@ -2,24 +2,15 @@ from sqlite3 import connect
 import logging
 from datetime import datetime
 from json import load
+from .user import UserProfile
+from .langs import not_enough_rights, profile_not_found
+from config import bot
+from telebot.types import ReactionTypeEmoji
 
 
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
 logger.addHandler(logging.FileHandler('logs.log'))
-
-
-class SqlRequest:
-    def __init__(self, database: str = 'database.sql', table: str = 'users'):
-        self.connection = connect(database)
-        self.cursor = self.connection.cursor()
-
-    def __del__(self):
-        self.cursor.close()
-        self.connection.close()
-
-    def select_one(self):
-        self.connection.execute()
 
 
 class GeoRequest:
@@ -28,6 +19,65 @@ class GeoRequest:
         self.latitude = float (message.location.latitude)
 
         self.in_rea = 37.62 <= self.longitude <= 37.64 and 55.72 <= self.latitude <= 55.74
+
+
+def basic_universal_logger(user: UserProfile, message) -> None:
+    name = user.user_name if user.exists else message.from_user.full_name
+    date = datetime.now().strftime("%d.%m.%Y %H:%M:%S")
+    _log = f'{date} | {name}: {message.text}'
+
+    logger.info(_log)
+    print(_log)
+
+
+def basic_cmd_logger(func):
+    def wrapper(message):
+        user = UserProfile(message.from_user.id)
+        basic_universal_logger(user, message)
+
+        if not user.exists or user.rights >= 1:
+            try:
+                func(message)
+            except AssertionError as expected_error_msg:
+                bot.reply_to(message, expected_error_msg, parse_mode='html')
+            except Exception as error_msg:
+                bot.reply_to(message, error_msg, parse_mode='html')
+                print(error_msg)
+        else:
+            bot.reply_to(message, not_enough_rights(message))
+
+    return wrapper
+
+
+def group_management_cmd_logger(func):
+    def wrapper(message):
+        user = UserProfile(message.from_user.id)
+        basic_universal_logger(user, message)
+
+        _incorrect_group_assets = ('Группа не указана')
+        
+        if not user.exists:
+            bot.reply_to(message, profile_not_found(message, True))
+        elif user.rights < 2:
+            bot.reply_to(message, not_enough_rights(message))
+        elif user.user_group in _incorrect_group_assets:
+            bot.reply_to(message, 'Не могу определить Вашу группу. Используйте /profile для её настройки и устранения этой ошибки.')
+        else:
+            try:
+                func(message)
+            except AssertionError as expected_error_msg:
+                bot.reply_to(message, expected_error_msg, parse_mode='html')
+            except Exception as error_msg:
+                bot.reply_to(message, error_msg, parse_mode='html')
+                print(error_msg)
+
+    return wrapper
+
+
+def emoji_reaction(message, emoji):
+    bot.set_message_reaction(chat_id=message.chat.id,
+                             message_id=message.id,
+                             reaction=[ReactionTypeEmoji(emoji=emoji)])
 
 
 def who_is_requestor (message):
@@ -48,11 +98,7 @@ def who_is_requestor (message):
     cur.close()
     conn.close()
 
-    logger.info(
-            f'{date}_________________________\n' +
-            f'{requestor} использовал следующую команду:\n' +
-            f' -> {message.text}\n' +
-            '____________________________________________\n')
+    logger.info(f'{date} | {requestor}: {message.text}\n')
 
     if message.chat.type == 'group':
         requestor = f'{message.chat.title} | {requestor}'
