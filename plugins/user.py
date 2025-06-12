ed1645241171a0de8bcbe3566e9823856e96ea33
Fortpsina <130063152+Fortpsina@users.noreply.github.com>
2025-06-12 19:58:51 +0300

    Update user.py

--- plugins/user.py
+++ plugins/user.py
@@ -1,7 +1,11 @@
 from sqlite3 import connect
 from datetime import datetime
 from threading import Lock
-from .langs import *
+
+if __name__ == '__main__':
+    from langs import *
+else:
+    from .langs import *
 
 
 USERS_STRUCT = """CREATE TABLE IF NOT EXISTS Users 
@@ -22,12 +26,12 @@ USERS_STRUCT = """CREATE TABLE IF NOT EXISTS Users
 HS_STRUCT = """CREATE TABLE IF NOT EXISTS Hs 
     (id INTEGER PRIMARY KEY,
     name TEXT,
-    structs TEXT,
+    groups TEXT,
     students INTEGER,
     founder_id INTEGER,
     longtitude REAL,
     lantitude REAL,
-    lesson_dates TEXT)"""
+    lessons TEXT)"""
 
 GROUP_STRUCT = """CREATE TABLE IF NOT EXISTS Groups 
     (id INTEGER PRIMARY KEY,
@@ -51,7 +55,9 @@ def create_table(table_type: str, db: str = 'database') -> None:
     _procedures = {"users": USERS_STRUCT,
                    "hs": HS_STRUCT,
                    "group": GROUP_STRUCT}
-    assert table_type in _procedures.keys(), "Указана несуществующая таблица."
+    
+    if not table_type in _procedures.keys():
+        raise ValueError("Указана несуществующая таблица.")
 
     conn = connect(f'{db}.sql')
     cur = conn.cursor()
@@ -83,7 +89,8 @@ class UserProfile:
         self.conn = connect('database.sql', check_same_thread=False)
         self.cur = self.conn.cursor()
 
-        assert isinstance(user_id, int), "ID указан неправильно, запрос отменён аварийно."
+        if not isinstance(user_id, int):
+            raise ValueError("ID указан неправильно, запрос отменён аварийно.")
 
         self.cur.execute(f'SELECT * FROM Users WHERE user_id = {user_id}')
         user_data = self.cur.fetchone()
@@ -110,16 +117,21 @@ class UserProfile:
         if column not in _allowed:
             return f"Неизвестная колонна. Доступные значения: {', '.join(_allowed)}"
         
+        if not self.exists:
+            return "Изменения не были внесены, поскольку профиль пользователя не найден."
+                
         db_lock = Lock()
         with db_lock:
+            self.cur.execute(f'SELECT id, {column} from Users WHERE user_id = {self.user_id}')
+            old_value = self.cur.fetchone()[1]
             self.cur.execute(f'UPDATE Users SET {column} = ? WHERE user_id = ?', (new_value, self.user_id))
             self.conn.commit()
 
-            return f"Значение изменено на {new_value}."
+            return f"Значение изменено с {old_value} на {new_value}."
         
-    def delete(self, physically: bool = False) -> str:
-        if not self.exists:
-            return PROFILE_NOT_FOUND_ERROR_NOT_OWN['ru']
+    def delete(self, physically: bool = False, message = None) -> str:
+        if message and not self.exists:
+            return profile_not_found(message)
         
         b_vklink = f'  Страница ВК: <code>{self.user_vk}</code>'
         b_tgid = f'  ID: <code>{self.user_id}</code>'
@@ -137,21 +149,75 @@ class UserProfile:
             self.conn.commit()
             return PROFILE_UTIL_DELETED_V['ru'] + f'{self.user_name}:\n{b_vklink}\n{b_tgid}\n{b_color}\n{b_reg}'
 
+    def ban(self):
+        return self.update('rights_level', 0)
+    
+    def unban(self):
+        return self.update('rights_level', 1)
+
     def __del__(self):
         self.cur.close()
         self.conn.close()
 
-class Username:
-    def __init__(self, message):
-        self.user_profile = UserProfile(message.from_user.id)
-        self.name = self.user_profile if self.user_profile.exists else message.from_user.full_name
 
-def register_user(message, **custom_data) -> None:
+class Hs:
+    def __init__(self, name_or_founder: str | int):
+        self.conn = connect('database.sql', check_same_thread=False)
+        self.cur = self.conn.cursor()
+
+        self.name_or_founder = name_or_founder
+        self.column_for_clue = ('name', 'founder_id')[isinstance(name_or_founder, int)]
+
+        self.cur.execute('SELECT * FROM Hs WHERE %s = "%s"' % (self.column_for_clue, name_or_founder))
+        self.all_data = self.cur.fetchone()
+
+        self.exists: bool = self.cur.rowcount == 1
+
+        if self.exists:
+            self.primary_id: int = self.all_data[0]
+            self.name: str = self.all_data[1]
+            self.groups: str = self.all_data[2]
+            self.students: int = self.all_data[3]
+            self.founder = UserProfile(int(self.all_data[4]))
+            self.longtitude: float = self.all_data[5]
+            self.lantitude: float = self.all_data[6]
+            self.lessons: str = self.all_data[7]
+
+    def update(self, column: str, new_value) -> str:
+        _allowed = [word.split()[0] for word in HS_STRUCT.splitlines()[2:]]
+        if column not in _allowed:
+            return f"Неизвестная колонна. Доступные значения: {', '.join(_allowed)}"
+        
+        if not self.exists:
+            return "Изменения не были внесены, поскольку организация не найдена."
+                
+        db_lock = Lock()
+        with db_lock:
+            self.cur.execute(f'SELECT id, {column} from Hs WHERE {self.column_for_clue} = {self.name_or_founder}')
+            old_value = self.cur.fetchone()[1]
+            self.cur.execute(f'UPDATE Users SET {column} = ? WHERE {self.column_for_clue} = ?', (new_value, self.name_or_founder))
+            self.conn.commit()
+
+            return f"Значение изменено с {old_value} на {new_value}."
+
+
+    def delete(self):
+        self.cur.execute()
+        self.conn.commit()
+
+
+    def __del__(self):
+        self.cur.close()
+        self.conn.close()
+
+
+
+def register_user(user_name: str, user_id: int, **custom_data) -> None:
     conn = connect('database.sql')
     cur = conn.cursor()
 
-    _name = custom_data.get("name",       message.from_user.full_name)
-    _id   = custom_data.get("user_id",    message.from_user.id)
+    _name = custom_data.get("name",       user_name)
+    _id   = custom_data.get("user_id",    user_id)
     _cnds = custom_data.get("conditions", "Н/Д")
     _hs   = custom_data.get("hs_name",    "УЗ не указано")
     _st   = custom_data.get("hs_struct",  "Структура УЗ не указана")
@@ -167,17 +233,26 @@ def register_user(message, **custom_data) -> None:
     cur.close()
     conn.close()
 
+def register_another_user(user_id) -> str:
+    new_profile = UserProfile(user_id)
+    
+    if new_profile.exists:
+        return "Пользователь с этим ID уже зарегестрирован."
+    
+    register_user("?", user_id)
+    return "Вы успешно создали пользователя."
 
-def register_hs(message, **custom_data) -> None:
-    conn = connect('database.sql')
+
+def register_hs(message) -> None:
+    conn = connect('database.sql', check_same_thread=False)
     cur = conn.cursor()
 
-    _n = custom_data.get("name",     message.text)
-    _v = custom_data.get("structs",  "")
-    _p = custom_data.get("students", "")
-    _i = custom_data.get("founder_id", message.from_user.id)
+    _n = message.text
+    _i = message.from_user.id
+    _l = ""
 
-    cur.execute(f'INSERT INTO Hs (name, structs, students, founder_id) VALUES (?, ?, ?, ?)', (_n, _v, _p, _i))
+    cur.execute(f'INSERT INTO Hs (name, groups, students, founder_id, longtitude, lantitude, lessons) VALUES (?, ?, ?, ?, ?, ?, ?)',
+                (_n, '', 1, _i, 0.0, 0.0, _l))
     conn.commit()
 
     cur.close()
@@ -200,7 +275,7 @@ def register_group(message, **custom_data) -> None:
     _i = custom_data.get("founder_id", message.from_user.id)
 
     cur.execute('INSERT INTO Groups (name, direction, students, mail_adress, mail_password, hs_name, hs_struct, prom, papers, founder_id) ' +
-                f'VALUES (?, ? ?, ?, ?, ?, ?, ?, ?)', (_n, _a, _v, _c, _h, _s, _g, _d, _p, _i))
+                f'VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)', (_n, _a, _v, _c, _h, _s, _g, _d, _p, _i))
     conn.commit()
 
     cur.close()
@@ -228,14 +303,3 @@ def users_list(db: str = 'database') -> str:
 
     else:
         return _to_reply[0:chunk_size] + "..."
-
-
-class CmdResponse:
-    def __init__(self, message):
-        self.requestor = UserProfile(message.from_user.id)
-
-    def log(self): pass
-
-
-if __name__ == '__main__':
-    print(UserProfile(324423).update("iujhugk", "jhkj"))
